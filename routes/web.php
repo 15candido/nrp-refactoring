929d6878fdb9ba73d3ce7e4ad4174cd8eecd51db
Cândido Silva <candido.da.silva.ca@gmail.com>
2023-01-28 12:03:09 +0000

    Views parameters and route model binding

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::get('/', function () {
     // });
     // $people = Person::latest()->get();
     return view('welcome', [
-        'people' =>Person::latest()->with('user')->get()
+        'people' => Person::latest()->with('user')->get()
     ]);
 });
 
@@ -26,28 +26,28 @@ Route::get('withuser', function () {
     return view('welcome', compact('people'));
 });
 
-Route::get('projects', function(){
+Route::get('projects', function () {
     // Give me All the Projects and the Collaborators that Collaborating in each one of the Project
     return view('projects', [
         'projects' => Project::latest()->with(['collaborator'])->get()
     ]);
 });
 
-Route::get('projects/{project:slug}', function (Project $project){
+Route::get('projects/{project:slug}', function (Project $project) {
     // Give me the Project where ID Matches this ID and  his collaborators 
     return view('project', [
-       'project' => $project
+        'project' => $project
     ]);
 });
 
-Route::get('collaborator/{collaborator:username}', function (Person $collaborator){
+Route::get('collaborator/{collaborator:username}', function (Person $collaborator) {
     // Give me the Collaborator where ID matches this ID and Return his Collaborative Projects
     return view('collaborator', [
-       'collaborator' => $collaborator
+        'collaborator' => $collaborator
     ]);
 });
 
-Route::get('demands', function(){
+Route::get('demands', function () {
     // Give me all the demands and their corresponding projects
     $demands = Demand::latest()->with('projects')->get();
     return view('demands', [
@@ -55,14 +55,14 @@ Route::get('demands', function(){
     ]);
 });
 
-Route::get('demands/{demand:slug}', function(Demand $demand){
+Route::get('demands/{demand:slug}', function (Demand $demand) {
     // Give me the demand where id matches this id and  his items 
     return view('demand', [
         'demand' => $demand
     ]);
 });
 
-Route::get('necessities', function(){
+Route::get('necessities', function () {
     // Give me All the Projects and the Collaborators that Collaborating in each one of the Project
     return view('items', [
         'items' => Item::latest()->with(['demands'])->get()
